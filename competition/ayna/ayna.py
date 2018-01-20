01ef0729f0bd701bf132893644fb875ca85f657a
M.Mucahid Benlioglu <m.mucahidbenlioglu@gmail.com>
2018-01-20 23:47:25 +0300

    ayna template

--- competition/ayna/ayna.py
+++ competition/ayna/ayna.py
@@ -16,7 +16,14 @@ def get_max_concat(words):
             scores[j][k] = get_score(words[j], words[k])
 
     # get max score
+    j, k = 0, 0
     max_score = 0
+    for j in xrange(len(words)):
+        for k in xrange(j + 1, len(words) - 1):
+            scores[j + 1][k + 1] += scores[j][k]
+        if scores[j][k + 1] > max_score:
+            max_score = scores[j][k + 1]
+    return max_score
 
 
 if __name__ == '__main__':
@@ -25,4 +32,4 @@ if __name__ == '__main__':
     for i in xrange(loop_cnt):
         strings.append(raw_input())
     strings.sort()
-    get_max_concat(strings)
+    print get_max_concat(strings)
