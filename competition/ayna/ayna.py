0f8b3a7117b92fe3cfdf4c8f17b6719f8443bdd7
M.Mucahid Benlioglu <m.mucahidbenlioglu@gmail.com>
2018-01-20 23:08:50 +0300

    ayna template

---
+++ competition/ayna/ayna.py
@@ -0,0 +1,28 @@
+"""
+    Created by mbenlioglu on 1/20/2018
+"""
+
+
+def get_score(str1, str2):
+    return 0
+
+
+def get_max_concat(words):
+    scores = [[0 for _ in xrange(len(words))] for _ in xrange(len(words))]
+
+    # set scores
+    for j in xrange(len(words)):
+        for k in xrange(j + 1, len(words)):
+            scores[i][j] = get_score(words[i], words[j])
+
+    # get max score
+    max_score = 0
+
+
+if __name__ == '__main__':
+    loop_cnt = int(raw_input())
+    strings = []
+    for i in xrange(loop_cnt):
+        strings.append(raw_input())
+    strings.sort()
+    get_max_concat(strings)
